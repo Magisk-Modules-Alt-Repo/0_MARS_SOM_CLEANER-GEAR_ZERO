d1783156b13c3e5a9ec8442359bc5ab25fcfe490
Marius Kopp <mariuskopp517@gmail.com>
2021-12-31 18:57:43 +0100

    V211231
    -Android 12 Ready
    (PHH AOSP GSI - VNDKLITE/SECURE ARM64 A/B)
    
    Happy New Year <3

--- MARS_SOM_CLEANER/customize.sh
+++ MARS_SOM_CLEANER/customize.sh
@@ -2,8 +2,14 @@
 
 MINAPI=30
 MAXAPI=31
-#DYNLIB=false
-#DEBUG=false
+DYNLIB=true
+DEBUG=false
+
+REPLACE="
+/system/app/Stk
+/system/product/app/Stk
+/system/product/priv-app/StorageManager
+"
 
 SKIPUNZIP=1
 unzip -qjo "$ZIPFILE" 'common/functions.sh' -d $TMPDIR >&2
